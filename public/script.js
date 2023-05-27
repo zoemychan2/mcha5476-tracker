ee2a1c831bb94e447e04356fcec3bcebe79d7cef
Your Name <zoemychan2@gmail.com>
2023-05-27 21:50:00 +1000

    Boba Tracker with Local Storage function

--- public/script.js
+++ public/script.js
@@ -29,9 +29,6 @@ function checkButton() {
 bobatrackform.addEventListener('submit', function (event) {
     event.preventDefault(); // Block default submission behaviour
 
-    //everything that's happening within the function is called here
-    console.log(bobatrackform.elements);
-
     //addTask function being called with our input elements
     addBobaorder(
         bobatrackform.elements.bobaOrder.value,
@@ -45,62 +42,67 @@ bobatrackform.addEventListener('submit', function (event) {
 
 
 // * THE DISPLAY EVERYTIME USER SUBMITS FORM - doing it as part of addBobaorder function *
-function displayTrackedbobaorders(bobatrackform) {
+function displayTrackedbobaorders() {
 
-    let dateElement = document.createElement('p');
-    dateElement.classList.add("date-element");
+    trackedbobaorders.innerHTML = "<h2>Tracked Boba Orders</h2>";
+    let localTracked = JSON.parse(localStorage.getItem('tracked'));
+    if (localTracked !== null){
 
-
-    // Changing the format of date
-    const newformat = new Intl.DateTimeFormat('en-US',{
-        year:'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric'
-    })
-
-    const formatDate = newformat.format(new Date(bobatrackform.date));
-
-    dateElement.innerHTML = `<p> <strong>${formatDate}</strong></p>`;
+        localTracked.forEach((bobatrackform)=>{
+            let dateElement = document.createElement('p');
+            dateElement.classList.add("date-element");
 
 
-    let delButton = document.createElement('button'); // Creating a delete button for every time something is added to the list
-    let delButtonText = document.createTextNode('🗑️'); // What the text in the delete button says
-    delButton.appendChild(delButtonText); // Append text to the button
+            // Changing the format of date
+            const newformat = new Intl.DateTimeFormat('en-US',{
+                year:'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric'
+            })
+            
+            const formatDate = newformat.format(new Date(bobatrackform.date));
 
-    // This button will be associated with each individual item it's next to - we will have multiple event listeners that will be different from eachother
-    delButton.addEventListener('click', function (event) {
-        item.remove(); // When delete button is pressed, item element is removed 
-        dateDeletecontainer.remove();
-        trackedBobaorders.forEach(function (trackArrayElement, trackArrayIndex) { // Removes item element from trackedBobaorders array
-            if (trackArrayElement.id == item.getAttribute('data-id')) {
-                trackedBobaorders.splice(trackArrayIndex, 1)
-            }
-        })
-        console.log(trackedBobaorders);
-    })
+            dateElement.innerHTML = `<p> <strong>${formatDate}</strong></p>`;
 
-    var dateDeletecontainer = document.createElement('div');
-    dateDeletecontainer.classList.add("datedelete-container");
-    dateDeletecontainer.appendChild(dateElement);
-    dateDeletecontainer.appendChild(delButton);
 
-    dateDeletecontainer.addEventListener('click', function(){
-        if (item.style.display=== "none"){
-            item.style.display="block"; // Show the li elements
-        } else {
-            item.style.display = "none"; // Hide the li elements
-        }
-    });
+            let delButton = document.createElement('button'); // Creating a delete button for every time something is added to the list
+            let delButtonText = document.createTextNode('🗑️'); // What the text in the delete button says
+            delButton.appendChild(delButtonText); // Append text to the button
 
-    trackedbobaorders.appendChild(dateDeletecontainer);
+            // This button will be associated with each individual item it's next to - we will have multiple event listeners that will be different from eachother
+            delButton.addEventListener('click', function (event) {
+                item.remove(); // When delete button is pressed, item element is removed 
+                dateDeletecontainer.remove();
+                
+                localTracked = localTracked.filter((item)=> item.id !==bobatrackform.id);
+                localStorage.setItem('tracked', JSON.stringify(localTracked));
+            })
 
-     // List of items which shows in the Tracked Boba Orders section 
-     let item = document.createElement('li');
-     item.style.display="none"; // Hide the li element at first
-     item.innerHTML = `<p> <strong>Order: </strong>${bobatrackform.bobaorder} <br/> <strong>Shop: </strong>${bobatrackform.bobashop} <br/> <strong>Size: </strong>${bobatrackform.bobasize} <br/> <strong>Price: </strong>$${bobatrackform.bobaprice} <br/> <strong>Rating: </strong>${mapRatingText(bobatrackform.bobarating)} <br/> <strong>Comments: </strong>${bobatrackform.bobacomments}</p>`; // Populating item with some data from bobatrackform, assigning it with a value (HTML code)
-     trackedbobaorders.appendChild(item); // Put item into trackedbobaorders
+            var dateDeletecontainer = document.createElement('div');
+            dateDeletecontainer.classList.add("datedelete-container");
+            dateDeletecontainer.appendChild(dateElement);
+            dateDeletecontainer.appendChild(delButton);
+
+            dateDeletecontainer.addEventListener('click', function(){
+                if (item.style.display=== "none"){
+                    item.style.display="block"; // Show the li elements
+                } else {
+                    item.style.display = "none"; // Hide the li elements
+                }
+            });
+
+            trackedbobaorders.appendChild(dateDeletecontainer);
+
+            // List of items which shows in the Tracked Boba Orders section 
+            let item = document.createElement('li');
+            item.style.display="none"; // Hide the li element at first
+            item.innerHTML = `<p> <strong>Order: </strong>${bobatrackform.bobaorder} <br/> <strong>Shop: </strong>${bobatrackform.bobashop} <br/> <strong>Size: </strong>${bobatrackform.bobasize} <br/> <strong>Price: </strong>$${bobatrackform.bobaprice} <br/> <strong>Rating: </strong>${mapRatingText(bobatrackform.bobarating)} <br/> <strong>Comments: </strong>${bobatrackform.bobacomments}</p>`; // Populating item with some data from bobatrackform, assigning it with a value (HTML code)
+            trackedbobaorders.appendChild(item); // Put item into trackedbobaorders
+
+        }) // Closing bracket for for loop
+    } // Closing bracket for if statement
 }
 
 // Function to help map bobarating value to my desired text
@@ -121,11 +123,10 @@ function mapRatingText(rating) {
     }
 }
 
-// Create array called trackedBobaorders
-var trackedBobaorders = [];
-
 //Create function called addBobaorder
 function addBobaorder(bobaorder, bobashop, bobasize, bobaprice, bobarating, bobacomments, date) {
+
+    console.log(bobaorder);
 
     let bobatrackform = {
         bobaorder,
@@ -138,13 +139,28 @@ function addBobaorder(bobaorder, bobashop, bobasize, bobaprice, bobarating, boba
         date: new Date().toISOString(),
     }
 
-    trackedBobaorders.push(bobatrackform); // Push bobatrackform into trackedBobaorders
-    displayTrackedbobaorders(bobatrackform);
+    // fetching and parse localStorage value
+    let localTracked = JSON.parse(localStorage.getItem('tracked'));
+
+    if (localTracked == null){
+        localTracked = [bobatrackform];
+    } else {
+        // Check to see if there is an existing task
+        if (localTracked.find(element => element.id === bobatrackform.id)){
+            console.log('bobatrackform ID already exists');
+        } else {
+            localTracked.push(bobatrackform);
+        }
+
+    }
+
+    localStorage.setItem('tracked', JSON.stringify(localTracked));
+    displayTrackedbobaorders();
 
     console.log(bobatrackform);
 }
 
-
+displayTrackedbobaorders();
 
 
 
